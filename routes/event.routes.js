3823a4741fd93cb320dffd810023151982e3df20
Alejandro Garcia <agarc371@gmail.com>
2020-11-06 16:34:06 -0500

    add event routes, confirm functionality via postman testing

--- routes/event.routes.js
+++ routes/event.routes.js
@@ -2,30 +2,52 @@
 // APP NEEDS TO KNOW YOU CREATED A NEW ROUTE FILE, THAT'S THE ONLY WAY FOR IT TO KNOW WHICH ROUTES YOU WANT TO HIT
 
 const express = require("express");
-const authorRouter = express.Router();
+const eventRouter = express.Router();
 
-// ********* require Author model in order to use it for CRUD *********
-const Author = require("../models/Event.model");
+// ********* require Event model in order to use it for CRUD *********
+const Event = require("../models/Event.model");
 
 // ****************************************************************************************
-// POST route to create a new author in the DB
+// POST route to create a new event in the DB
 // ****************************************************************************************
 
-// <form action="/authors" method="POST">
-authorRouter.post("/api/authors", (req, res, next) => {
-  Author.create(req.body)
-    .then((authorDoc) => res.status(200).json({ author: authorDoc }))
+// <form action="/event" method="POST">
+eventRouter.post("/api/events", (req, res, next) => {
+  Event.create(req.body)
+    .then((createdEvent) => res.status(200).json({ event: createdEvent }))
     .catch((err) => next(err));
 });
 
 // ****************************************************************************************
-// GET all authors from the DB
+// GET all events from the DB
 // ****************************************************************************************
 
-authorRouter.get("/api/authors", (req, res, next) => {
-  Author.find() // <-- .find() method gives us always an ARRAY back
-    .then((authorsFromDB) => res.status(200).json({ authors: authorsFromDB }))
+eventRouter.get("/api/events", (req, res, next) => {
+  Event.find() // <-- .find() method gives us always an ARRAY back
+    .then((eventsFromDB) => res.status(200).json({ events: eventsFromDB }))
     .catch((err) => next(err));
 });
 
-module.exports = authorRouter;
+// ****************************************************************************************
+// POST route to save the updates
+// ****************************************************************************************
+
+// <form action="/events/{{foundEvent._id}}/update" method="POST">
+eventRouter.post("/api/events/:id/update", (req, res) => {
+  Event.findByIdAndUpdate(req.params.id, req.body, { new: true })
+    .then((updatedEvent) => res.status(200).json({ event: updatedEvent }))
+    .catch((err) => next(err));
+});
+
+// ****************************************************************************************
+// POST route to delete the event
+// ****************************************************************************************
+
+// <form action="/event/{{this._id}}/delete" method="post">
+eventRouter.post("/api/events/:eventId/delete", (req, res) => {
+  Event.findByIdAndRemove(req.params.eventId)
+    .then(() => res.json({ message: "Successfully removed!" }))
+    .catch((err) => next(err));
+});
+
+module.exports = eventRouter;
